f8a3c8f7c8607312c43bd96e694010ee5d96fe56
avilacartagenaj <avilacartagenaj@gmail.com>
2026-09-10 16:40:20 -0500

    Implementación de métodos (desde isEmpty hasta los 2 Insert)

--- src/MiListaDoble.java
+++ src/MiListaDoble.java
@@ -1,52 +1,128 @@
 public class MiListaDoble implements ListInterface {
+    DoubleNode cabeza = null;
+
     @Override
     public boolean isEmpty() {
-        return false;
+        return cabeza == null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        DoubleNode iterador = cabeza;
+        int contador = 0;
+
+        while (iterador != null) {
+            contador++;
+            iterador = iterador.siguiente;
+        }
+        return contador;
     }
 
     @Override
     public void clear() {
-
+        cabeza = null;
     }
 
     @Override
     public Object getHead() {
-        return null;
+        if (cabeza == null) {
+            return null;
+        }
+        return cabeza.dato;
     }
 
     @Override
     public Object getTail() {
-        return null;
+        if (cabeza == null) {
+            return null;
+        }
+
+        DoubleNode iterador = cabeza;
+        while (iterador.siguiente != null) {
+            iterador = iterador.siguiente;
+        }
+        return iterador.dato;
     }
 
     @Override
     public Object get(DoubleNode node) {
-        return null;
+        if (node == null) {
+            return null;
+        }
+        return node.dato;
     }
 
     @Override
     public DoubleNode search(Object object) {
+        if (object == null) {
+            return null;
+        }
+
+        DoubleNode iterador = cabeza;
+        while (iterador != null) {
+            if (iterador.dato.equals(object)) {
+                return iterador;
+            }
+            iterador = iterador.siguiente;
+        }
         return null;
     }
 
     @Override
     public boolean add(Object object) {
-        return false;
+        if (object == null) return false;
+
+        DoubleNode nuevo = new DoubleNode(object);
+
+        if (cabeza == null) {
+            cabeza = nuevo;
+            return true;
+        }
+
+        DoubleNode actual = cabeza;
+        while (actual.siguiente != null) {
+            actual = actual.siguiente;
+        }
+
+        actual.siguiente = nuevo;
+        nuevo.anterior = actual;
+
+        return true;
     }
 
     @Override
     public boolean insert(DoubleNode node, Object object) {
+        if (node == null || object == null) return false;
+
+        DoubleNode actual = cabeza;
+        while (actual != null) {
+            if (actual == node) {
+                DoubleNode nuevo = new DoubleNode(object);
+                DoubleNode siguienteOriginal = node.siguiente;
+
+                nuevo.anterior = node;
+                nuevo.siguiente = siguienteOriginal;
+                node.siguiente = nuevo;
+
+                if (siguienteOriginal != null) {
+                    siguienteOriginal.anterior = nuevo;
+                }
+
+                return true;
+            }
+            actual = actual.siguiente;
+        }
         return false;
     }
 
     @Override
     public boolean insert(Object objectRef, Object object) {
-        return false;
+        DoubleNode nodoReferencia = search(objectRef);
+
+        if (nodoReferencia == null) {
+            return false;
+        }
+        return insert(nodoReferencia, object);
     }
 
     @Override
